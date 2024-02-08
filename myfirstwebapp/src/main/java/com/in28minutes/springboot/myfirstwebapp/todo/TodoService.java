cb5e5387e31970a0e4d5dd3c35f76daa331934d2
dgsim <dgsim126@gmail.com>
2024-02-08 11:37:55 +0900

    105, step 24 (delete todo)

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -29,5 +30,11 @@ public class TodoService {
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
 		Todo todo= new Todo(++todosCount, username, description, targetDate, done);
 		todos.add(todo);
+	}
+	
+	public void deleteById(int id) {
+		// todo.getId() == id (해당 조건일 때 삭제)
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
 	}
 }
